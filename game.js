a42b87a479e14675426eb9c05419db87c98c9faa
Jeremy-GameHive <jeremy.moshe@gamehive.com>
2021-11-30 16:48:46 -0500

    init

--- game.js
+++ game.js
@@ -1,176 +1,99 @@
-const CONFIG = {
-  floorHeight: 100,
-}
+(function initialize() {
+  let options = document.getElementsByClassName('option-container')[0];
 
-const Colors = {
-  DBLUE: 0x4E6CC6,
-  WHITE: 0xFDFDFD,
-  SHADOW: 0,
-  PANEL: 0xf199cc,
-}
-
-let overlay = new PIXI.Graphics();
-let title = new PIXI.Text("Spin Sample",{fontsize: 40});
-let style = -1;
-let button;
-
-function initGame() {
-  // create background
-  let back = new PIXI.Graphics();
-  back.beginFill(0x002266).drawRect(0, 0, appRect.width, appRect.height);
-  back.interactive = true;
-  // back.addListener("pointermove", onMoveOut);
-
-  // create grass
-  let floor = new PIXI.Graphics();
-  floor.beginFill(0x00ff00).lineStyle(1).drawRect(0, 0, appRect.width, CONFIG.floorHeight);
-  floor.y = appRect.height - CONFIG.floorHeight;
-
-  overlay.beginFill(0).drawRect(0, 0, appRect.width, appRect.height);
-  overlay.alpha = 0;
-  app.stage.addChild(back, floor, title, overlay);
-
-  title.position.set((appRect.width - title.width) / 2, (CONFIG.floorHeight - title.height) / 2);
-
-  startAnimation({x: 200, y: 300});
-}
-
-window.addEventListener('keydown', e => {
-  switch(e.key){ 
-    case ' ': this.startAnimation({x: 200, y: 300}); break;
-    case '1': this.startAnimation({x: 100, y: 200}); break;
-    case '2': this.startAnimation({x: 200, y: 200}); break;
-    case '3': this.startAnimation({x: 300, y: 200}); break;
-    case '4': this.startAnimation({x: 400, y: 200}); break;
+  for (let i = 1; i <= 6; i++) {
+    let button = document.createElement('button');
+    button.classList.add('option-button');
+    
+    button.appendChild(makeImage(i));
+    let index = i;
+    button.addEventListener('pointerdown', () => addChatEntry(index));
+    options.appendChild(button);
   }
-});
-// . 3232342
-function startAnimation(center) {
-  let triangles = [];
-  let vector = {
-    angle: 0,
-    distance: 30,
-    color: 0xffff00,
-    alpha: 0,
-  };
-  let bubble = new Bubble;
-  app.stage.addChild(bubble);
-  bubble.position.set(center.x, center.y);
+})();
 
-  let bubble2 = new Bubble;
-  app.stage.addChild(bubble2);
-  bubble2.position.set(center.x, center.y);
-  
-  for (let i = 0; i < 4; i++) {
-    let triangle = new TriangleArrow();
-    app.stage.addChild(triangle);
-    triangles.push(triangle);
+let addChatEntry = (function() {
+  let chatbox = document.getElementById('chatbox');
+  let MAX_LENGTH = 10;
+  let entries = [];
+
+   return function addChatEntry(stickerIndex) {
+    let div = document.createElement('div');
+    div.classList.add('chat-entry');
+
+    let header = document.createElement('div');
+    header.classList.add('namefield');
+    header.innerHTML = '<div>You</div><div>Member</div>';
+    div.appendChild(header);
+
+    let image = makeImage(stickerIndex);
+    div.appendChild(image);
+    
+    chatbox.appendChild(div);
+    entries.push(div);
+
+    if (entries.length > MAX_LENGTH) chatbox.removeChild(entries.shift());
+
+    chatbox.scrollTop = chatbox.scrollHeight;
+
+    animate(image, stickerIndex);
+    // image.rotation = 200;
   }
+})();
 
-  let setPositions = () => {
-    triangles.forEach((triangle, i) => {
-      let angle = vector.angle + Math.PI * 2 * i / 4;
-      triangle.rotation = angle - Math.PI / 2;
-      triangle.position.set(center.x + vector.distance * Math.cos(angle), center.y + vector.distance * Math.sin(angle));
-      triangle.tint = vector.color;
-      triangle.alpha = vector.alpha;
-    });
-  }
+function makeImage(stickerIndex) {
+  let imageName = `Sticker${stickerIndex}.png`;
 
-  setPositions();
+  let image = document.createElement('img');
+  image.classList.add('chat-image');
+  image.src = imageName;
 
-  bubble.width = bubble.height = 0;
-  bubble.alpha = 0;
-  bubble.tint = 0;
-
-  bubble2.width = bubble2.height = 200;
-  bubble2.alpha = 0;
-  bubble2.tint = 0;
-
-  // COLOR VALUES
-  let colorBubble1 = 0x00ff00;
-  let colorBubble2 = 0x00ff00;
-
-  let colorArrow1 = 0xffff00;
-  let colorArrow2 = 0x00ff00;
-  // let colorArrow3 = 0xffcc00;
-  let colorArrowFlash = 0xffffff;
-
-  // TIMING VALUES
-  let timeScale = 1;
-
-  let intro = 1000 * timeScale;
-  let main = 1000 * timeScale;
-  let outro = main * 0.25;
-
-  // bubble scaling (main timing)
-  new JMTween(bubble, intro).to({width: 100, height: 100}).easing(Easing.Linear.In).start()
-    .chain(bubble, 100).to({alpha: 0})
-    // .chain(bubble, outro).wait(main * 0.9 - intro * 0.25).to({width: 0, height: 0}).easing(Easing.Back.In)
-    // .onComplete(() => bubble.destroy());
-
-  // bubble alpha
-  new JMTween(bubble, 1).to({alpha: 1}).start();
-
-  // bubble colour
-  new JMTween(bubble, intro / 3).colorTo({tint: colorBubble1}).start()
-    .chain(bubble, intro / 3).wait(intro / 3).colorTo({tint: colorBubble2})
-    .chain(bubble, outro / 3).wait(main * 0.9).colorTo({tint: colorBubble1})
-    .chain(bubble, outro / 3).wait(outro / 3).colorTo({tint: 0});
-
-      // bubble scaling (main timing)
-  new JMTween(bubble2, intro).to({width: 100, height: 100}).easing(Easing.Linear.In).start()
-  .chain(bubble2, 300).to({alpha: 0})
-  // .chain(bubble2, outro).wait(main * 0.9 - intro * 0.25).to({width: 0, height: 0}).easing(Easing.Back.In)
-  // .onComplete(() => bubble2.destroy());
-
-// bubble2 alpha
-new JMTween(bubble2, intro).to({alpha: 1}).start();
-
-// bubble2 colour
-new JMTween(bubble2, intro / 3).colorTo({tint: colorBubble1}).start()
-  .chain(bubble2, intro / 3).wait(intro / 3).colorTo({tint: colorBubble2})
-  .chain(bubble2, outro / 3).wait(main * 0.9).colorTo({tint: colorBubble1})
-  .chain(bubble2, outro / 3).wait(outro / 3).colorTo({tint: 0});
-
-  // triangle spin (main timing)
-  let spinSpeed = 1000;
-  new JMTween(vector, main + outro).wait(intro).to({angle: Math.PI * spinSpeed / main}).start().onUpdate(setPositions)
-    // .chain(vector, outro).to({angle: Math.PI * 1}).onUpdate(setPositions)
-    .onComplete(() => triangles.forEach(triangle => triangle.destroy()));
-
-  // triangle distance
-  new JMTween(vector, 1).wait(intro).to({distance: 50}).start()
-    .chain(vector, outro / 2).wait(main).to({distance: 10})
-    .chain(vector, outro / 2).to({distance: 70});
-
-  // triangle alpha
-  new JMTween(vector, 100).wait(intro).to({alpha: 1}).start()
-  .chain(vector, outro / 2).wait(main).to({alpha: 0});
-
-  // triangle colour
-  vector.color = colorArrow1;
-  new JMTween(vector, main * 0.3).wait(intro).colorTo({color: colorArrow2}).easing(Easing.Quadratic.In).start()
-    .chain(vector, main * 0.04).wait(main * 0.4).colorTo({color: colorArrowFlash}).easing(Easing.Quadratic.In)
-    .yoyo(true, 2);
+  return image;
 }
 
-class TriangleArrow extends PIXI.Graphics {
-  constructor() {
-    super();
-    this.beginFill(0xffffff).lineStyle(1);
-    this.moveTo(0, 0).lineTo(10, 15).lineTo(-10, 15).lineTo(0, 0);
+function animate(sticker, index) {
+  console.log('index', index);
+  let obj = {height: 1, width: 1, rotation: 0, x: 0, y: 0};
+  switch(index) {
+    case 1:
+      obj.y = 50;
+      obj.height = 0;
+      new JMTween({percent: 0},2000).onUpdate(() => applyTransform(sticker, obj)).onComplete(() => sticker.style.transform = '').start();
+      new JMTween(obj, 300).to({height: 1, y: -20, rotation: 30}).easing(Easing.Back.Out).start()
+        .chain(obj, 100).wait(200).to({rotation: 20}).yoyo(true, 3)
+        .chain(obj, 300).wait(100).to({height: 1, rotation: 0, y: 0});
+
+      // new JMTween(obj, 300).wait(400).to({rotation: -45}).start();  
+      break;
+    case 2:
+      obj.rotation = 700;
+      obj.height = 0.3;
+      obj.width = 0.3;
+      new JMTween(obj, 500).to({rotation: 0}).start().onUpdate(data => applyTransform(sticker, data))
+      .onComplete(() => sticker.style.transform = '');
+      new JMTween(obj, 300).to({height: 0.8, width: 0.8}).easing(Easing.Quadratic.InOut).start()
+        .chain(obj, 200).to({height: 1, width: 1});
+        ``
+      break;
+    case 3:
+      obj.width = 0.1;
+      obj.height = 0.1;
+      new JMTween({percent: 0},2000).onUpdate(() => applyTransform(sticker, obj)).onComplete(() => sticker.style.transform = '').start();
+      new JMTween(obj, 500).to({width: 1.3, height: 1.3, rotation: 15}).easing(Easing.Back.Out).start()
+        .chain(obj, 1000).wait(500).to({width: 1, height: 1, rotation: 0})
+      break;
+    default:
+      obj.height = 0;
+      obj.width = 0;
+      new JMTween(obj, 700).to({height: 1, width: 1}).easing(Easing.Back.Out).start()
+      .onUpdate(data => applyTransform(sticker, data))
+      .onComplete(() => sticker.style.transform = '');
+      break;
   }
 }
 
-class Bubble extends PIXI.Graphics {
-  constructor() {
-    super();
-    let easing = Easing.Circular.In;
-    let alphaRatio = 0.02;
-    let length = 60;
-    for (let i = 100; i > length; i--) {
-      this.lineStyle(1, 0xffffff, easing((i - length) * alphaRatio)).drawCircle(0, 0, i);
-    }
-  }
+function applyTransform(sticker, data) {
+  sticker.style.transform = `scaleX(${data.width}) scaleY(${data.height}) translateX(${data.x}px) translateY(${data.y}px) rotate(${data.rotation}deg)`;
 }
+
+// chatbox.appendChild(makeChatEntry(1));
